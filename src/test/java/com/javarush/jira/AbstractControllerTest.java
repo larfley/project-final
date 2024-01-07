f843276b78cfb96029d70e861be3daf17e3600cc
Oleg Vrublevskiy <larfley@gmail.com>
2024-01-08 00:20:53 +0300

    Task#1 - done Task#2 - done(removed social Task#3 - done(Moved sensitive data to a separate file)

--- src/test/java/com/javarush/jira/AbstractControllerTest.java
+++ src/test/java/com/javarush/jira/AbstractControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
